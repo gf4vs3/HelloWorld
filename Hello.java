ef35a1fbeb1f4e693d835f5a8c2d4b7aa13981a5
gf4vs3 <137363372+gf4vs3@users.noreply.github.com>
2024-02-06 14:49:49 -0500

    I completed my code

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*************************************
+ *Project:  Alexander Zalewski 
+ *Date:  2/6
+ *Program Name:  Hello.java
+**************************************/ 
+
+public class Hello{
+
+    public static void main(String[] args){
+        System.out.println("Hello everyone!");
+        System.out.println("I wrote my first java program"); 
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
